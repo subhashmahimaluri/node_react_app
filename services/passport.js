5a38db07064fba025eb3ea504349104fc49c7334
Subhash MAhimaluri <subhash.mahimaluri@mindtree.com>
2017-08-15 11:01:21 +0530

    reverting full path

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: 'https://stark-fjord-19182.herokuapp.com/auth/google/callback',
+      callbackURL: '/auth/google/callback',
       proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
